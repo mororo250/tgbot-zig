5bb34c2cf8b92fd88bfd16f4671e3335db3af4a0
mororo250 <joao_marcos_8@hotmail.com>
2025-08-30 18:50:08 -0300

    Initial zig project with custom test runner

---
+++ build.zig
@@ -0,0 +1,47 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const mod = b.addModule("tgbot_zig", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "tgbot_zig",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "tgbot_zig", .module = mod },
+            },
+        }),
+    });
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const mod_tests = b.addTest(.{
+    .name = "tgbot_zig_tests",
+    .root_module = mod,
+    .test_runner = .{.path = b.path("src/test_runner.zig"), .mode = .simple },
+});
+    b.installArtifact(mod_tests);
+    const run_mod_tests = b.addRunArtifact(mod_tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_mod_tests.step);
+
+    if (b.args) |args| {
+        run_mod_tests.addArgs(args);
+        run_cmd.addArgs(args);
+    }
+}
