e48eaba5791223618d22f63c260f684faed29c83
mororo250 <joao_marcos_8@hotmail.com>
2025-08-31 16:58:48 -0300

    Template zig project

--- build.zig
+++ build.zig
@@ -3,21 +3,24 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const mod = b.addModule("tgbot_zig", .{
+    const mod = b.addModule("lib_mod", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
+        .optimize = optimize
     });
-
-    const exe = b.addExecutable(.{
-        .name = "tgbot_zig",
-        .root_module = b.createModule(.{
+    const exe_mod = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
             .imports = &.{
                 .{ .name = "tgbot_zig", .module = mod },
             },
-        }),
+        });
+
+
+    const exe = b.addExecutable(.{
+        .name = "exe_template",
+        .root_module = exe_mod
     });
 
     b.installArtifact(exe);
@@ -30,10 +33,11 @@ pub fn build(b: *std.Build) void {
     run_cmd.step.dependOn(b.getInstallStep());
 
     const mod_tests = b.addTest(.{
-    .name = "tgbot_zig_tests",
-    .root_module = mod,
-    .test_runner = .{.path = b.path("src/test_runner.zig"), .mode = .simple },
-});
+        .name = "zig_tests",
+        .root_module = mod,
+        .test_runner = .{.path = b.path("src/test_runner.zig"), .mode = .simple },
+    });
+
     b.installArtifact(mod_tests);
     const run_mod_tests = b.addRunArtifact(mod_tests);
 
@@ -44,4 +48,18 @@ pub fn build(b: *std.Build) void {
         run_mod_tests.addArgs(args);
         run_cmd.addArgs(args);
     }
+
+    const check = b.addExecutable(.{
+        .name = "Check",
+        .root_module = exe_mod,
+    });
+
+    // This is a test of making zls work in tests.
+    const check_test = b.addTest(.{
+        .root_module = mod,
+    });
+
+    const check_step = b.step("check", "Check for zls analysis");
+    check_step.dependOn(&check.step);
+    check_step.dependOn(&check_test.step);
 }
